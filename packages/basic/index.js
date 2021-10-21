8d58dfd8ab32a6c5c28e0af82b1cc8726a061e6d
Yang Guang <yorg@foxmail.com>
2021-10-22 00:31:22 +0800

    feat: basic project structure

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -1,3 +1,79 @@
 module.exports = {
-    
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'standard',
+    'plugin:import/recommended',
+  ],
+  settings: {
+    'import/resolver': {
+      node: { extensions: ['.js', '.mjs', '.ts', '.d.ts'] },
+    },
+  },
+  overrides: [
+    {
+      files: ['*.d.ts'],
+      rules: {
+        'import/no-duplicates': 'off',
+      },
+    },
+  ],
+  rules: {
+    // common
+    'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+    'camelcase': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'curly': ['error', 'multi-or-nest', 'consistent'],
+    'no-cond-assign': ['error', 'always'],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'quote-props': ['error', 'consistent-as-needed'],
+
+    // best-practice
+    'block-scoped-var': 'error',
+    'eqeqeq': ['error', 'smart'],
+    'no-alert': 'warn',
+
+    // es6
+    'arrow-parens': [
+      'error',
+      'as-needed',
+      {
+        requireForBlockBody: true,
+      },
+    ],
+    'no-var': 'error',
+    'object-shorthand': [
+      'error',
+      'always',
+      {
+        ignoreConstructors: false,
+        avoidQuotes: true,
+      },
+    ],
+    'prefer-arrow-callback': [
+      'error',
+      {
+        allowNamedFunctions: false,
+        allowUnboundThis: true,
+      },
+    ],
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: true,
+      },
+    ],
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+
+    // import
+    'import/order': 'error',
+    'import/first': 'error',
+    'import/no-mutable-exports': 'error',
+  },
 }
