1e55ab157ea1473dc0198269f8e7a68b6a2177d5
杨光 <yangguang6@corp.netease.com>
2021-10-25 10:59:33 +0800

    feat: vue config

---
+++ packages/vue/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@guang/eslint-config-ts',
+  ],
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      rules: {
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+  ],
+}
