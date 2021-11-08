1e035e45a052df27400a0d57d52d411d41fabe88
杨光 <yangguang6@corp.netease.com>
2021-11-08 16:40:00 +0800

    feat: add vue2 config

---
+++ .changeset/wise-hotels-yawn.md
@@ -0,0 +1,5 @@
+---
+"@yangguang6/eslint-config-vue2": patch
+---
+
+vue2 config
---
+++ packages/vue2/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: [
+    'plugin:vue/recommended',
+    '@yangguang6/eslint-config-basic',
+  ],
+}
